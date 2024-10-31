a3f6d3aebf0a12dd82cc2c0c5c7dff1ed7629154
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2024-10-31 18:19:37 -0500

    Clean repository Files

--- Files/WebJob/cronWebJob.ps1
+++
@@ -1,9 +0,0 @@
-#
-# cronWebJob.ps1
-#
-
-$cronFile = "$($env:HOME)\site\wwwroot\cron.php"
-
-$phpExe = "${env:ProgramFiles(x86)}\PHP\v7.3\php.exe"
-Start-Process -NoNewWindow -FilePath $phpExe -ArgumentList @($cronFile)
-
